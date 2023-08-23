32e7f657f2f8d83e728bd94ed943c5fd386929f4
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-08-22 22:33:01 -0400

    Create Brightness.lua

---
+++ Brightness.lua
@@ -0,0 +1,1 @@
+YOURTABHERE:AddSlider('brightnessSlider', {Text = 'Brightness',Default = 2.5,Min = 0.00,Max = 20,Rounding = 1,Compact = true,Callback = function(value)game.Lighting.Brightness = value end})
