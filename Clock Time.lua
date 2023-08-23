ed0aa42b4f490c049a624644b19ff18832e72ef3
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-08-22 22:35:27 -0400

    Update Clock Time.lua

--- Clock Time.lua
+++ Clock Time.lua
@@ -1,2 +1,5 @@
+local toggleState = false
+local clockTime = 12
+YOURTABHERE:AddToggle('TC', {Text = 'Time',Default = false,Callback = function(state) toggleState = state end})
 YOURTABHERE:AddSlider('ClockTimeSlider', {Text = 'Current Time',Default = clockTime,Min = 0.00,Max = 24.00,Rounding = 1,Compact = true,Callback = function(value)clockTime = value if toggleState then game:GetService("Lighting").ClockTime = clockTime end end})
 game:GetService("Lighting"):GetPropertyChangedSignal("ClockTime"):Connect(function() if toggleState then game:GetService("Lighting").ClockTime = clockTime end end)
