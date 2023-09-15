5347b5764c82aea835a39ad77408cc27b6cd2f17
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-09-15 06:43:41 -0400

    Update LibRemover.lua

--- LibRemover.lua
+++ LibRemover.lua
@@ -1,21 +1,18 @@
-        YOURTABHERE:AddDropdown('RemovalDropdown', {Values = { 'None','RightLeg',"RightArm",'LeftLeg',"LeftArm","HummanoidRootPart","Torso","Head"},Default = 1,Multi = false, Text = 'Remove Hitboxes', Tooltip = 'Removes LocalPlayer Body Parts',})
-        Options.RemovalDropdown:OnChanged(function()
-            if Options.RemovalDropdown.Value == "None" then
-            elseif Options.RemovalDropdown.Value == "RightLeg" then
-                game.Players.LocalPlayer.Character.RightUpperLeg:Destroy()
-            elseif Options.RemovalDropdown.Value == "LeftLeg" then
-                game.Players.LocalPlayer.Character.LeftUpperLeg:Destroy()
-             elseif Options.RemovalDropdown.Value == "LeftArm" then
-                game.Players.LocalPlayer.Character.LeftUpperArm:Destroy()
-             elseif Options.RemovalDropdown.Value == "RightArm" then
-                game.Players.LocalPlayer.Character.RightUpperArm:Destroy()
-             elseif Options.RemovalDropdown.Value == "Torso" then
-                LocalPlayer.Character.UpperTorso.Waist:Destroy() 
-             elseif Options.RemovalDropdown.Value == "HummanoidRootPart" then
-                game.Players.LocalPlayer.Character.HummanoidRootPart:Destroy()
-             elseif Options.RemovalDropdown.Value == "Head" then
-                game.Players.LocalPlayer.Character.Head:Destroy()
-             end
-      end)
---goofy asf
-YOURTABHERE:AddButton('Nill-Char', function() game.Players.LocalPlayer.Character.LowerTorso.Root:Destroy() end)
+ExploitsTab:AddDropdown("RemovalDropdown",{
+        Values = {"None", "RightLeg", "LeftLeg", "LeftArm", "RightArm", "Torso", "HummanoidRootPart", "Head"},
+        Default = 1,
+        Multi = false,
+        Text = "Remove Hitboxes",
+        Tooltip = "Removes LocalPlayer Body Parts"
+    }
+)
+Options.RemovalDropdown:OnChanged(
+    function()
+        local bodyPartName = bodyPartMap[Options.RemovalDropdown.Value]
+        if bodyPartName then
+            local bodyPart = game.Players.LocalPlayer.Character:FindFirstChild(bodyPartName)
+            if bodyPart then
+                bodyPart:Destroy()
+            end
+        end
+    end)
